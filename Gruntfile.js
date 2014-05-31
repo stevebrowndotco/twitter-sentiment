b43405f95ab19d1250b7b1f71c1550294997804b
Steve Brown <contactstevebrown@gmail.com>
2014-05-31 16:57:37 +0100

    mean.co was too excessive for what I need, just use node instead

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,126 +1,48 @@
 'use strict';
 
-var paths = {
-    js: ['*.js', 'server/**/*.js', 'public/**/*.js', 'test/**/*.js', '!test/coverage/**', '!public/system/lib/**', 'packages/**/*.js', '!packages/**/node_modules/**'],
-    html: ['public/**/views/**', 'server/views/**', 'packages/**/public/**/views/**', 'packages/**/server/views/**'],
-    css: ['public/**/css/*.css', '!public/system/lib/**', 'packages/**/public/**/css/*.css']
-};
+module.exports = function (grunt) {
+  // Show elapsed time at the end
+  require('time-grunt')(grunt);
+  // Load all grunt tasks
+  require('load-grunt-tasks')(grunt);
 
-module.exports = function(grunt) {
-
-    if (process.env.NODE_ENV !== 'production') {
-        require('time-grunt')(grunt);
+  // Project configuration.
+  grunt.initConfig({
+    nodeunit: {
+      files: ['test/**/*_test.js']
+    },
+    jshint: {
+      options: {
+        jshintrc: '.jshintrc',
+        reporter: require('jshint-stylish')
+      },
+      gruntfile: {
+        src: 'Gruntfile.js'
+      },
+      lib: {
+        src: ['lib/**/*.js']
+      },
+      test: {
+        src: ['test/**/*.js']
+      }
+    },
+    watch: {
+      gruntfile: {
+        files: '<%= jshint.gruntfile.src %>',
+        tasks: ['jshint:gruntfile']
+      },
+      lib: {
+        files: '<%= jshint.lib.src %>',
+        tasks: ['jshint:lib', 'nodeunit']
+      },
+      test: {
+        files: '<%= jshint.test.src %>',
+        tasks: ['jshint:test', 'nodeunit']
+      }
     }
+  });
 
-    // Project Configuration
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
-        assets: grunt.file.readJSON('server/config/assets.json'),
-        clean: ['public/build'],
-        watch: {
-            js: {
-                files: paths.js,
-                tasks: ['jshint'],
-                options: {
-                    livereload: true
-                }
-            },
-            html: {
-                files: paths.html,
-                options: {
-                    livereload: true
-                }
-            },
-            css: {
-                files: paths.css,
-                tasks: ['csslint'],
-                options: {
-                    livereload: true
-                }
-            }
-        },
-        jshint: {
-            all: {
-                src: paths.js,
-                options: {
-                    jshintrc: true
-                }
-            }
-        },
-        uglify: {
-            options: {
-                mangle: false
-            },
-            production: {
-                files: '<%= assets.js %>'
-            }
-        },
-        csslint: {
-            options: {
-                csslintrc: '.csslintrc'
-            },
-            src: paths.css
-        },
-        cssmin: {
-            combine: {
-                files: '<%= assets.css %>'
-            }
-        },
-        nodemon: {
-            dev: {
-                script: 'server.js',
-                options: {
-                    args: [],
-                    ignore: ['public/**', 'node_modules/**'],
-                    ext: 'js,html',
-                    nodeArgs: ['--debug'],
-                    delayTime: 1,
-                    env: {
-                        PORT: require('./server/config/config').port
-                    },
-                    cwd: __dirname
-                }
-            }
-        },
-        concurrent: {
-            tasks: ['nodemon', 'watch'],
-            options: {
-                logConcurrentOutput: true
-            }
-        },
-        mochaTest: {
-            options: {
-                reporter: 'spec',
-                require: 'server.js'
-            },
-            src: ['test/mocha/**/*.js', 'packages/**/test/mocha/**/*.js']
-        },
-        env: {
-            test: {
-                NODE_ENV: 'test'
-            }
-        },
-        karma: {
-            unit: {
-                configFile: 'test/karma/karma.conf.js'
-            }
-        }
-    });
+  // Default task.
+  grunt.registerTask('default', ['jshint', 'nodeunit']);
 
-    //Load NPM tasks
-    require('load-grunt-tasks')(grunt);
-
-    //Default task(s).
-    if (process.env.NODE_ENV === 'production') {
-        grunt.registerTask('default', ['clean','cssmin', 'uglify', 'concurrent']);
-    } else {
-        grunt.registerTask('default', ['clean','jshint', 'csslint', 'concurrent']);
-    }
-
-    //Test task.
-    grunt.registerTask('test', ['env:test', 'mochaTest', 'karma:unit']);
-
-    // For Heroku users only.
-    // Docs: https://github.com/linnovate/mean/wiki/Deploying-on-Heroku
-    grunt.registerTask('heroku:production', ['cssmin', 'uglify']);
 };
---
+++ test/twitter-sentiment-api_test.js
@@ -0,0 +1,36 @@
+'use strict';
+
+var twitterSentimentApi = require('../lib/twitter-sentiment-api.js');
+
+/*
+  ======== A Handy Little Nodeunit Reference ========
+  https://github.com/caolan/nodeunit
+
+  Test methods:
+    test.expect(numAssertions)
+    test.done()
+  Test assertions:
+    test.ok(value, [message])
+    test.equal(actual, expected, [message])
+    test.notEqual(actual, expected, [message])
+    test.deepEqual(actual, expected, [message])
+    test.notDeepEqual(actual, expected, [message])
+    test.strictEqual(actual, expected, [message])
+    test.notStrictEqual(actual, expected, [message])
+    test.throws(block, [error], [message])
+    test.doesNotThrow(block, [error], [message])
+    test.ifError(value)
+*/
+
+exports.twitterSentimentApi = {
+  setUp: function(done) {
+    // setup here
+    done();
+  },
+  'no args': function(test) {
+    test.expect(1);
+    // tests here
+    test.equal(twitterSentimentApi.awesome(), 'awesome', 'should be awesome.');
+    test.done();
+  }
+};
